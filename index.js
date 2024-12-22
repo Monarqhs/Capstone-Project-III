823c03019775c454e748a10bb39e932132bf2961
Monarqhs <ghalihyulio12@gmail.com>
2024-12-22 11:54:28 +0700

    second commit, update port in index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/delete/:id', (req, res) => {
 });
 
 // Start Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
